5c15cb659a5a5b64f60682dad549b6974bef267e
Imammahdi170625 <imammahdizakaria75@gmail.com>
2022-08-23 14:03:21 +0600

    sgd modified

--- batch (1).py
+++ batch (1).py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def sigmoid(z):
@@ -12,9 +13,9 @@ X = np.array([[0, 0, 1],
 
 D = np.array([[0, 0, 1, 1]])
 W = np.random.rand(1, 3)
-W = np.array([0.398153445313372, 0.781806505071597, 0.918582850410889])
 ALPHA = 0.9
 dWsum = np.zeros((3, 1))
+error_list = []
 for i in range(100):
     for j in range(D.shape[1]):
         x = np.reshape(X[j], (3, 1))
@@ -22,6 +23,8 @@ for i in range(100):
         v = np.dot(W, x)
         y = sigmoid(v)
         e = d - y
+        if j == 2:
+            error_list.append(e[0][0])
         delta = y * (1 - y) * e
         dW = ALPHA * delta * x
         dWsum = dWsum + dW
@@ -29,8 +32,18 @@ for i in range(100):
     dWavg = np.divide(dWsum, 4)
     W = W + np.transpose(dWavg)
 
+print(W)
+
 for i in range(D.shape[1]):
     x = np.reshape(X[i], (3, 1))
     v = np.dot(W, x)
     y = sigmoid(v)
-    print(np.round(y))
+    print(y)
+
+fig, ax = plt.subplots()
+fig.suptitle("error graph")
+bx = []
+for i in range(100):
+    bx.append(i)
+ax.plot(bx, error_list)
+plt.show()
--- help.py
+++ help.py
@@ -1,7 +1,1 @@
-import numpy as np
-a = [1,4,2,3]
-b = np.reshape(a,(4,1))
-s = np.array([[2,4,3],[3,5,6]])
-print(s[0])
-print(a)
-print(b)
+
